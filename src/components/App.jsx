88118551298cf219867fd248c78956f46cd1cfce
michal-wolski <michal.wolski130@gmail.com>
2022-06-15 21:20:06 +0200

    Change App structure from class to hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getImages } from './services/api';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -6,71 +6,124 @@ import LoaderComponent from './Loader/Loader';
 import Modal from './Modal/Modal';
 import Button from './Button/Button';
 
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    page: 1,
-    isModalOpen: false,
-    isLoading: false,
-  };
-  openModal = modalImage => {
-    this.setState({ isModalOpen: true, modalImage });
-    window.addEventListener('keydown', this.closeModal);
+const App = () => {
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const openModal = modalImage => {
+    setIsModalOpen(true);
+    // <modalImage />;
+    window.addEventListener('keydown', closeModal);
   };
 
-  closeModal = evt => {
+  const closeModal = evt => {
     if (evt.target === evt.currentTarget || evt.keyCode === 27)
-      this.setState({ isModalOpen: false });
-    window.removeEventListener('keydown', this.closeModal);
+      setIsModalOpen(false);
+    window.removeEventListener('keydown', closeModal);
   };
 
-  handleSubmit = query => {
-    this.setState({ query, isLoading: true });
+  const handleSubmit = query => {
+    setQuery(query);
+    setIsLoading(true);
     getImages(query)
       .then(resData => {
-        this.setState({ images: resData.data.hits });
+        setImages({ images: resData.data.hits });
       })
-      .finally(() => this.setState({ isLoading: false }));
+      .finally(() => setIsLoading(false));
   };
 
-  changePage = () => {
-    const { query, page } = this.state;
+  const changePage = () => {
     getImages(query, page + 1).then(resData => {
-      this.setState(prevState => ({
-        page: prevState.page + 1,
-        images: [...prevState.images, ...resData.data.hits],
-      }));
+      setPage(page + 1);
+      setImages([...resData.data.hits]);
     });
   };
-  componentDidMount() {
-    const { query, page } = this.state;
 
-    this.handleSubmit(query, page);
-  }
+  // useEffect(() => {
+  //   handleSubmit(query, page);
+  // }),
+  //   [];
 
-  componentDidUpdate(prevProps, prevState) {
-    const { page } = this.state;
-    if (prevState.page !== page) {
-      window.scrollTo({
-        top: document.documentElement.scrollHeight,
-        behavior: 'smooth',
-      });
-    }
-  }
+  return (
+    <>
+      <Searchbar handleSubmit={handleSubmit} />
+      {isLoading && <LoaderComponent />}
+      <ImageGallery images={images} openModal={openModal} />
+      {images.length > 0 && <Button changePage={changePage} />}
+      {isModalOpen && <Modal closeModal={closeModal} />}
+    </>
+  );
+};
 
-  render() {
-    const { images, isModalOpen, modalImage, isLoading } = this.state;
-    return (
-      <>
-        <Searchbar handleSubmit={this.handleSubmit} />
-        {isLoading && <LoaderComponent />}
-        <ImageGallery images={images} openModal={this.openModal} />
-        {images.length > 0 && <Button changePage={this.changePage} />}
-        {isModalOpen && (
-          <Modal modalImage={modalImage} closeModal={this.closeModal} />
-        )}
-      </>
-    );
-  }
-}
+export default App;
+// export class App extends Component {
+//   state = {
+//     query: '',
+//     images: [],
+//     page: 1,
+//     isModalOpen: false,
+//     isLoading: false,
+//   };
+//   openModal = modalImage => {
+//     this.setState({ isModalOpen: true, modalImage });
+//     window.addEventListener('keydown', this.closeModal);
+//   };
+
+//   closeModal = evt => {
+//     if (evt.target === evt.currentTarget || evt.keyCode === 27)
+//       this.setState({ isModalOpen: false });
+//     window.removeEventListener('keydown', this.closeModal);
+//   };
+
+//   handleSubmit = query => {
+//     this.setState({ query, isLoading: true });
+//     getImages(query)
+//       .then(resData => {
+//         this.setState({ images: resData.data.hits });
+//       })
+//       .finally(() => this.setState({ isLoading: false }));
+//   };
+
+//   changePage = () => {
+//     const { query, page } = this.state;
+//     getImages(query, page + 1).then(resData => {
+//       this.setState(prevState => ({
+//         page: prevState.page + 1,
+//         images: [...prevState.images, ...resData.data.hits],
+//       }));
+//     });
+//   };
+//   componentDidMount() {
+//     const { query, page } = this.state;
+
+//     this.handleSubmit(query, page);
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const { page } = this.state;
+//     if (prevState.page !== page) {
+//       window.scrollTo({
+//         top: document.documentElement.scrollHeight,
+//         behavior: 'smooth',
+//       });
+//     }
+//   }
+
+//   render() {
+//     const { images, isModalOpen, modalImage, isLoading } = this.state;
+//     return (
+//       <>
+//         <Searchbar handleSubmit={this.handleSubmit} />
+//         {isLoading && <LoaderComponent />}
+//         <ImageGallery images={images} openModal={this.openModal} />
+//         {images.length > 0 && <Button changePage={this.changePage} />}
+//         {isModalOpen && (
+//           <Modal modalImage={modalImage} closeModal={this.closeModal} />
+//         )}
+//       </>
+//     );
+//   }
+// }
