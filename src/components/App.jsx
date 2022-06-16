a92e58f94a985a14c74d1fedc558816b09f07768
michal-wolski <michal.wolski130@gmail.com>
2022-06-16 22:10:10 +0200

    Remove old code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { getImages } from './services/api';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -44,10 +44,6 @@ const App = () => {
     });
   };
 
-  // useEffect(() => {
-  //   handleSubmitSetQuery('car', 1);
-  // }, []);
-
   return (
     <>
       <Searchbar handleSubmitSetQuery={handleSubmitSetQuery} />
@@ -60,71 +56,3 @@ const App = () => {
 };
 
 export default App;
-// export class App extends Component {
-//   state = {
-//     query: '',
-//     images: [],
-//     page: 1,
-//     isModalOpen: false,
-//     isLoading: false,
-//   };
-//   openModal = modalImage => {
-//     this.setState({ isModalOpen: true, modalImage });
-//     window.addEventListener('keydown', this.closeModal);
-//   };
-
-//   closeModal = evt => {
-//     if (evt.target === evt.currentTarget || evt.keyCode === 27)
-//       this.setState({ isModalOpen: false });
-//     window.removeEventListener('keydown', this.closeModal);
-//   };
-
-//   handleSubmit = query => {
-//     this.setState({ query, isLoading: true });
-//     getImages(query)
-//       .then(resData => {
-//         this.setState({ images: resData.data.hits });
-//       })
-//       .finally(() => this.setState({ isLoading: false }));
-//   };
-
-//   changePage = () => {
-//     const { query, page } = this.state;
-//     getImages(query, page + 1).then(resData => {
-//       this.setState(prevState => ({
-//         page: prevState.page + 1,
-//         images: [...prevState.images, ...resData.data.hits],
-//       }));
-//     });
-//   };
-//   componentDidMount() {
-//     const { query, page } = this.state;
-
-//     this.handleSubmit(query, page);
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     const { page } = this.state;
-//     if (prevState.page !== page) {
-//       window.scrollTo({
-//         top: document.documentElement.scrollHeight,
-//         behavior: 'smooth',
-//       });
-//     }
-//   }
-
-//   render() {
-//     const { images, isModalOpen, modalImage, isLoading } = this.state;
-//     return (
-//       <>
-//         <Searchbar handleSubmit={this.handleSubmit} />
-//         {isLoading && <LoaderComponent />}
-//         <ImageGallery images={images} openModal={this.openModal} />
-//         {images.length > 0 && <Button changePage={this.changePage} />}
-//         {isModalOpen && (
-//           <Modal modalImage={modalImage} closeModal={this.closeModal} />
-//         )}
-//       </>
-//     );
-//   }
-// }
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -47,47 +47,6 @@ const Searchbar = ({ handleSubmitSetQuery }) => {
   );
 };
 
-// export default class Searchbar extends Component {
-//   state = {
-//     text: '',
-//   };
-
-//   onChange = evt => {
-//     const { value } = evt.target;
-//     this.setState({ text: value });
-//   };
-
-//   handleSubmit = evt => {
-//     evt.preventDefault();
-//     const { handleSubmit } = this.props;
-//     handleSubmit(this.state.text);
-//   };
-
-//   render() {
-//     const { text } = this.state;
-//     return (
-//       <div>
-//         <header className={s.Searchbar}>
-//           <form onSubmit={this.handleSubmit} className={s.SearchForm}>
-//             <button type="submit" className={s.SearchForm_button}>
-//               <span className={s.SearchForm_button_label}>Search</span>
-//             </button>
-
-//             <input
-//               className={s.SearchForm_input}
-//               onChange={this.onChange}
-//               type="text"
-//               defaultValue={text}
-//               autoComplete="off"
-//               autoFocus
-//               placeholder="Search images and photos"
-//             />
-//           </form>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
 export default Searchbar;
 
 Searchbar.propTypes = {
